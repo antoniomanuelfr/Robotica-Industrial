8d5f1a492306deff30e71d0c9784340c06605072
Antonio Manuel Fresneda Rodriguez <antoniomanuelfr@gmail.com>
2019-05-25 12:19:23 +0200

    Practica 3

---
+++ Practica 3/apartado1.m
@@ -0,0 +1,49 @@
+% % Sub-apartado 1
+t= 0:0.01:20;
+
+fprintf('Modelo Sub-amortiguado\n');
+
+sim('modelo_subA',20)
+[tp, sobr, te] = parametros(youtSub,t);
+fprintf('Tiempo de pico = %4.2f\n',tp);
+fprintf('Sobreoscilacion = %4.2f por ciento\n',sobr);
+fprintf('Tiempo de establecimiento al 2 por ciento = %4.2f\n\n',te);
+
+fprintf('Modelo Sobre-amortiguado\n');
+
+sim('modelo_sobA',20)
+[tp, sobr, te] = parametros(youtSob,t);
+fprintf('Tiempo de pico = %4.2f\n',tp);
+fprintf('Sobreoscilacion = %4.2f por ciento\n',sobr);
+fprintf('Tiempo de establecimiento al 2 por ciento = %4.2f\n\n',te);
+
+fprintf('Modelo criticamente amortiguado\n');
+
+sim('modelo_criA',20)
+[tp, sobr, te] = parametros(youtCri,t);
+fprintf('Tiempo de pico = %4.2f\n',tp);
+fprintf('Sobreoscilacion = %4.2f por ciento\n',sobr);
+fprintf('Tiempo de establecimiento al 2 por ciento = %4.2f\n',te);
+f1 = figure(1);
+plot(t, [youtSob,youtSub,youtCri]);
+legend('Modelo sobre-alimentado', 'Modelo sub-amortiguado', 'modelo criticamente amortiguado');
+title ('Respuesta del sistema con los distintos modelos')
+xlabel('Tiempo')
+ylabel('y(t)')
+
+% sub-apartado 2
+f2=figure(2);
+plot (t,youtSub,'DisplayName','Sin ceros')
+hold on;
+a=[1,0.5,0.2,0.1,0.05];
+for i = 1:1:length(a)
+    b=a(i);
+    sim('modelo_Ceros',20)
+    txt = ['Cero en ',num2str(b)];
+    plot(t,yout2,'DisplayName',txt);
+end
+hold off
+title ('Respuesta del sistema con distintos ceros')
+xlabel('Tiempo')
+ylabel('y(t)')
+legend()
---
+++ Practica 3/parametros.m
@@ -0,0 +1,16 @@
+% Funcion para calcular los parametros de un sistema
+function [ tp, sobr, te ] = parametros(yout, t)
+    [M,I] = max(yout);
+    tp = t(I);
+    % Restamos uno porque sabemos que la entrada es un escalon unitario
+    sobr = (M-1)*100;
+    err = 0.02;
+    min_value = 1-err;
+    max_value = 1+err;
+    for a = length(yout):-1:1
+       if yout(a) < min_value || yout(a) > max_value
+            te = t(a-1);
+            break;
+       end
+    end 
+end
